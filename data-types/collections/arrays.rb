4ccb8fad7adcddd442b0c2dff4a96406abfbc172
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-05-12 09:53:48 -0600

    Work on arrays

--- data-types/collections/arrays.rb
+++ data-types/collections/arrays.rb
@@ -3,91 +3,87 @@ require 'minitest/pride'
 
 class ArraysTest < Minitest::Test
   def test_0
-    skip
+
     # In the line below, create a new empty array
-    empty = ______
+    empty = []
     assert_equal [], empty
   end
 
   def test_1
-    skip
+
     # In the line below, create and array with the numbers 1 - 5
-    nums = ______
+    nums = (1..5).to_a
     assert_equal [1,2,3,4,5], nums
   end
 
   def test_2
-    skip
     nums = [1,2,3]
     # In the line below, call a method on the nums variable
     # defined above to access the second element
-    actual = nums.______
+    actual = nums[1]
     expected = 2
     assert_equal expected, actual
   end
 
   def test_22
-    skip
+
     # In the line below, call a method on the nums variable
     # defined above to access the last element
     nums = [1,2,3]
-    actual = nums
+    actual = nums[2]
     expected = 3
     assert_equal expected, actual
 
     # Now try to find another way to achieve the same effect
     nums = [1,2,3]
-    actual = nums
+    actual = nums.last
     expected = 3
     assert_equal expected, actual
   end
 
   def test_3
-    skip
+
     hummus = ["tahini", "chickpeas", "lemons"]
     # Call a method on the hummus variable that will
     # tell us how many elements there are in the array
-    assert_equal 3, hummus._____
+    assert_equal 3, hummus.length
   end
 
   def test_4
-    skip
+
     world_cup = ["Germany", "Mexico", "Iceland", "Portugal"]
     # In the line below, add the element "Brazil" to the end of the world_cup array
-
-    assert_equal "Brazil", world_cup.last
+    world_cup << "Brazil"
+    assert_equal "Brazil", world_cup.last 
 
     # Use a different method to add the element "Japan" to the end of the array
-
+    world_cup.push('Japan')
     assert_equal "Japan", world_cup.last
   end
 
   def test_a
-    skip
     world_cup = ["Germany", "Mexico", "Iceland", "Portugal"]
     # Call a method on the world_cup variable to remove and return
     # the last element of the array
-    last_element = world_cup
+    last_element = world_cup.pop
     assert_equal "Portugal", last_element
     assert_equal ["Germany", "Mexico", "Iceland"], world_cup
   end
 
   def test_5
-    skip
     karaoke = ["Shake it Off", "Dancing Queen", "Bohemian Rhapsody"]
     # Call a method on the karaoke variable to ask whether "Toxic"
     # is an element or not
-    toxic_in_array = karaoke
+    toxic_in_array = karaoke.include?("Toxic")
     assert_equal false, toxic_in_array
 
     # Now call a method on the karaoke variable to ask whether "Dancing Queen"
     # is an element or not
-    dancing_queen_in_array = karaoke
+    dancing_queen_in_array = karaoke.include?("Dancing Queen")
     assert_equal true, dancing_queen_in_array
   end
 
   def test_b
-    skip
     band = ["Guitar", "Drums", "Bass"]
     # Call a method on the band variable to add the element "Vocals"
     # to the beginning of the array
